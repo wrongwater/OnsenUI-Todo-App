4f372927770608d4182647b0c7b70a5d3a3ed92a
Fran Dios <frankdiox@gmail.com>
2017-08-03 12:42:36 +0900

    Simplify item creation.

--- js/services.js
+++ js/services.js
@@ -12,8 +12,7 @@ myApp.services = {
     // Creates a new task and attaches it to the pending task list.
     create: function(data) {
       // Task item template.
-      var template = document.createElement('div');
-      template.innerHTML =
+      var taskItem = ons.createElement(
         '<ons-list-item tappable category="' + myApp.services.categories.parseId(data.category)+ '">' +
           '<label class="left">' +
            '<ons-checkbox></ons-checkbox>' +
@@ -25,10 +24,8 @@ myApp.services = {
             '<ons-icon style="color: grey; padding-left: 4px" icon="ion-ios-trash-outline, material:md-delete"></ons-icon>' +
           '</div>' +
         '</ons-list-item>'
-      ;
+      );
 
-      // Takes the actual task item.
-      var taskItem = template.firstChild;
       // Store data within the element.
       taskItem.data = data;
 
@@ -120,8 +117,7 @@ myApp.services = {
       var categoryId = myApp.services.categories.parseId(categoryLabel);
 
       // Category item template.
-      var template = document.createElement('div');
-      template.innerHTML =
+      var categoryItem = ons.createElement(
         '<ons-list-item tappable category-id="' + categoryId + '">' +
           '<div class="left">' +
             '<ons-radio name="categoryGroup" input-id="radio-'  + categoryId + '"></ons-radio>' +
@@ -130,10 +126,7 @@ myApp.services = {
             (categoryLabel || 'No category') +
           '</label>' +
         '</ons-list-item>'
-      ;
-
-      // Takes the actual category item.
-      var categoryItem = template.firstChild;
+      );
 
       // Adds filtering functionality to this category item.
       myApp.services.categories.bindOnCheckboxChange(categoryItem);
